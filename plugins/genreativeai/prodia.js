fe8b54e71f47fe632ee8e1295ce9c2ffb2714d0f
galalio445 <theworldh95@gmail.com>
2024-02-14 19:06:56 +0500

    new fix

--- plugins/genreativeai/prodia.js
+++ plugins/genreativeai/prodia.js
@@ -13,13 +13,13 @@ exports.run = {
         Func
     }) => {
         try {
-            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'a girl'), m)
-            client.sendReact(m.chat, '🕒', m.key)
+            const prompt = "beautiful girl";
             const negative_prompt = "blury, bad quality";
             const model = "absolutereality_v181.safetensors [3d9d4d2b]";
-            const json = await rsnchat.prodia(text, negative_prompt, model)
-            const buffer = Buffer.from(json.base64)
-            client.sendFile(m.chat, buffer, 'image.jpg', `◦  *Prompt* : ${text}`, m)
+
+            rsnchat.prodia(prompt, negative_prompt, model).then((response) => {
+                console.log(response);
+            });
         } catch (e) {
             return client.reply(m.chat, global.status.error, m)
         }
