04164741db09a32c72787c80433e206a80242800
galalio445 <theworldh95@gmail.com>
2024-02-18 20:42:23 +0500

    s

--- plugins/ai/chatgpt2.js
+++ plugins/ai/chatgpt2.js
@@ -1,43 +1,43 @@
 const { OpenAI } = require('openai');
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // Access API key from environment variable
+    apiKey: process.env.OPENAI_API_KEY, // Access API key from environment variable
 });
 
 exports.run = {
-  usage: ['chatgpt'],
-  use: 'query',
-  category: 'ai',
-  async: async (m, {
-    client,
-    text,
-    args,
-    isPrefix,
-    command,
-    Func
-  }) => {
-    try {
-      if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
-      client.sendReact(m.chat, '🕒', m.key)
-      const openai = new OpenAI({apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
-    });
-    async function main() {
-      const json = await openai.chat.create({
-        model: 'gpt-3.5-turbo',
-        messages: [{
-          role: 'user',
-          content: text
-        }],
-      });
-    }
-      client.reply(m.chat, json.data.messages[0].content, m);
-    } catch (e) {
-      console.error('Error:', e);
-      return client.reply(m.chat, global.status.error, m);
-    }
-  },
-  error: false,
-  limit: true,
-  verified: true,
-  premium: true,
+    usage: ['chatgpt'],
+    use: 'query',
+    category: 'ai',
+    async: async (m, {
+        client,
+        text,
+        args,
+        isPrefix,
+        command,
+        Func
+    }) => {
+        try {
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
+            client.sendReact(m.chat, '🕒', m.key)
+            async function main() {
+                const json = await openai.chat.create({
+                    model: 'gpt-3.5-turbo',
+                    messages: [{
+                        role: 'user',
+                        content: text
+                    }],
+                });
+            }
+            const result = await main();
+            client.reply(m.chat, result.data.messages[0].content, m);
+        } catch (e) {
+            console.error('Error:', e);
+            // Handle the error, e.g., send an error message to the client
+            return client.reply(m.chat, global.status.error, m);
+        }
+    },
+    error: false,
+    limit: true,
+    verified: true,
+    premium: true,
 };
