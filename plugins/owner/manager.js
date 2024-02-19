6c7b0effda6314d26b8072e8aeee23f9bac8bd76
galalio445 <theworldh95@gmail.com>
2024-02-19 14:29:15 +0500

    fix

--- plugins/owner/manager.js
+++ plugins/owner/manager.js
@@ -59,12 +59,12 @@ exports.run = {
             let banned = is_user.filter(v => v.banned).length
             client.reply(m.chat, `乂  *U N B A N N E D*\n\n*“Succesfully removing @${jid.split`@`[0]} from banned list.”*\n\n*Total : ${banned}*`, m)
          } else if (command == 'chatban') { // banned user
-            let banned = global.db.groups[m.chat].banned = true;
+            let banned = global.db.groups.banned = true;
             client.reply(m.chat, `乂  *C H A T B A N N E D*\n\n*Successfully*`, m)
          }
          else if (command == 'chatunban') { // unbanned user
            if (!global.db.groups[m.chat].banned) return client.reply(m.chat, Func.texted('bold', `🚩 Target not banned.`), m)
-             let banned = global.db.groups[m.chat].banned = false;
+             let banned = global.db.groups.banned = false;
             client.reply(m.chat, `乂  *C H A T  U N B A N N E D*\n\n*“Succesfully*`, m)
          } 
       } catch (e) {
