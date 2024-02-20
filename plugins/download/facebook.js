d801fdffb9717f6d42bdd9c925a7e6b8638665e0
galalio445 <theworldh95@gmail.com>
2024-02-20 19:34:36 +0500

    asfd

--- plugins/download/facebook.js
+++ plugins/download/facebook.js
@@ -17,11 +17,11 @@ exports.run = {
          client.sendReact(m.chat, '🕒', m.key)
          const json = facebook.v1(args[0])
          if (json.isHdAvailable === true) {
-            client.sendFile(m.chat, json.urls[0].hd,  json.title + 'mp4', `◦ *Quality* : HD`, m)
+            client.sendFile(m.chat, json.urls.hd,  json.title + 'mp4', `◦ *Quality* : HD`, m)
          } else {
-            let result = json.url[0]
+            let result = json.urls
             if (!result) return client.reply(m.chat, global.status.fail, m)
-            client.sendFile(m.chat,json.urls[0].sd, json.title + 'mp4', `◦ *Quality* : SD`, m)
+            client.sendFile(m.chat,json.urls.sd, json.title + 'mp4', `◦ *Quality* : SD`, m)
          }
       } catch (e) {
          console.log(e)
