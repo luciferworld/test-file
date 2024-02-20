4d0bab3966e30c666bee5bc9ab68a144a78aa306
galalio445 <theworldh95@gmail.com>
2024-02-20 17:30:47 +0500

    fix

--- plugins/ephoto.js
+++ plugins/ephoto.js
@@ -11,9 +11,10 @@ exports.run = {
        Func
     }) => {
        try {
-        await w5botapi.ephoto2("https://ephoto360.com/hieu-ung-chu-tren-nen-cat-trang-tuyet-dep-663.html", ["text"])
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err));
+        if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'lucifer'), m)
+       const json = await w5botapi.ephoto2("https://ephoto360.com/hieu-ung-chu-tren-nen-cat-trang-tuyet-dep-663.html", [`${text}`])
+       if (!json.status) return client.reply(m.chat, global.status.fail, m)
+       client.sendFile(m.chat, json.image, ``, `🍟 *Process* : ${((new Date - old) * 1)} ms`, m)
        } catch (e) {
           console.log(e)
           return client.reply(m.chat, Func.jsonFormat(e), m)
