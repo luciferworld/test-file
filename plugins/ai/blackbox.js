f5c3a79cfc89ecc0c2d8b6e30f5abd7dc01aa8ba
galalio445 <theworldh95@gmail.com>
2024-02-20 16:56:45 +0500

    sdfsdf

--- plugins/ai/blackbox.js
+++ plugins/ai/blackbox.js
@@ -1,30 +1,26 @@
 exports.run = {
-  usage: ['blackbox'],
-  use: 'query',
-  category: 'ai',
-  async async (m, {
-    client,
-    text,
-    args,
-    isPrefix,
-    command,
-    Func
-  }) {
-    try {
-      if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m);
-      
-      const processingMessage = await m.reply('Processing your request...' ); // Send placeholder message
-
-      const json = await Func.fetchJson(`https://api.betabotz.eu.org/api/search/blackbox-chat?text=${text}&apikey=beta-Ibrahim1209`);
-      
-      await client.sendMessageModify(m.chat, processingMessage, { content: json.message }); // Edit placeholder message with result
-    } catch (e) {
-      console.error(e);
-      return client.reply(m.chat, global.status.error, m);
-    }
-  },
-  error: false,
-  limit: true,
-  verified: true,
-  premium: false,
-};
+    usage: ['blackbox'],
+    use: 'query',
+    category: 'ai',
+    async: async (m, {
+      client,
+      text,
+      args,
+      isPrefix,
+      command,
+      Func
+    }) => {
+       try {
+        if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
+        client.sendReact(m.chat, '🕒', m.key)
+        const json = await Func.fetchJson(`https://api.betabotz.eu.org/api/search/blackbox-chat?text=${text}&apikey=beta-Ibrahim1209`)
+        client.reply(m.chat, json.message, m)
+        } catch (e) {
+      return client.reply(m.chat, global.status.error, m)
+  }
+},
+error: false,
+limit: true,
+verified: true,
+premium: false,
+}
