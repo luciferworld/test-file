f4c1163d8297bf3007af958ca3a2c18bb2e2fae7
galalio445 <theworldh95@gmail.com>
2024-02-20 19:15:31 +0500

    Feat: adding scrapper

--- plugins/download/facebook.js
+++ plugins/download/facebook.js
@@ -1,3 +1,5 @@
+const { facebook } = require("@xct007/frieren-scraper");
+
 exports.run = {
    usage: ['fb'],
    hidden: ['fbdl', 'fbvid'],
@@ -11,28 +13,29 @@ exports.run = {
       Func
    }) => {
       try {
-         if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://fb.watch/7B5KBCgdO3'), m)
-         if (!args[0].match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/)) return client.reply(m.chat, global.status.invalid, m)
-         client.sendReact(m.chat, '🕒', m.key)
-         const json = await Api.neoxr('/fb', {
-            url: args[0]
-         })
-         if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
-         let result = json.data.find(v => v.quality == 'HD')
-         if (result) {
-            client.sendFile(m.chat, result.url, Func.filename('mp4'), `◦ *Quality* : HD`, m)
+         if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://fb.watch/7B5KBCgdO3'), m);
+         if (!args[0].match(/(?:https?:\/\/(web\.|www\.|m\.)?(facebook|fb)\.(com|watch)\S+)?$/)) return client.reply(m.chat, global.status.invalid, m);
+         
+         client.sendReact(m.chat, '🕒', m.key);
+         const json = await facebook.v1(args[0]);
+         
+         let hdResult = json.url[0].find(v => v.quality == 'hd');
+         let sdResult = json.url[0].find(v => v.quality == 'sd');
+
+         if (hdResult) {
+            client.sendFile(m.chat, hdResult.hd, json.title + '.mp4', `◦ *Quality* : HD`, m);
+         } else if (sdResult) {
+            client.sendFile(m.chat, sdResult.sd, json.title + '.mp4', `◦ *Quality* : SD`, m);
          } else {
-            let result = json.data.find(v => v.quality == 'SD')
-            if (!result) return client.reply(m.chat, global.status.fail, m)
-            client.sendFile(m.chat, result.url, Func.filename('mp4'), `◦ *Quality* : SD`, m)
+            return client.reply(m.chat, global.status.fail, m);
          }
       } catch (e) {
-         console.log(e)
-         return client.reply(m.chat, global.status.error, m)
+         console.log(e);
+         return client.reply(m.chat, global.status.error, m);
       }
    },
    error: false,
    limit: true,
    cache: true,
    location: __filename
-}
+};
