82f8d9cd1e3a1d9915f2f329443271945f533454
galalio445 <theworldh95@gmail.com>
2024-02-20 16:41:44 +0500

    ltttlr test

--- plugins/ai/blackbox.js
+++ plugins/ai/blackbox.js
@@ -12,9 +12,9 @@ exports.run = {
   }) => {
      try {
       if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
-      const processingMessage = await client.reply(m.chat, "Processing your request..."); // Send placeholder message
+      const processingMessage = await m.reply("Processing your request..."); // Send placeholder message
       const json = await Func.fetchJson(`https://api.betabotz.eu.org/api/search/blackbox-chat?text=${text}&apikey=beta-Ibrahim1209`);
-      await client.editMessage(m.chat, processingMessage.key.id, json.message); // Edit placeholder message with result
+      await client.modifyChat(m.chat, processingMessage.id, json.message); // Edit placeholder message with result
       } catch (e) {
         console.error(e);
         return client.reply(m.chat, global.status.error, m);
