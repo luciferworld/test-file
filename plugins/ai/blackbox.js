2829df8196fc386670e1c4825c92dc49915f6c8e
galalio445 <theworldh95@gmail.com>
2024-02-20 16:48:58 +0500

    test feature

--- plugins/ai/blackbox.js
+++ plugins/ai/blackbox.js
@@ -2,26 +2,29 @@ exports.run = {
   usage: ['blackbox'],
   use: 'query',
   category: 'ai',
-  async: async (m, {
+  async async (m, {
     client,
     text,
     args,
     isPrefix,
     command,
     Func
-  }) => {
-     try {
-      if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
-      const processingMessage = await m.reply("Processing your request..."); // Send placeholder message
+  }) {
+    try {
+      if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m);
+      
+      const processingMessage = await client.sendMessage(m.chat, 'Processing your request...'); // Send placeholder message
+
       const json = await Func.fetchJson(`https://api.betabotz.eu.org/api/search/blackbox-chat?text=${text}&apikey=beta-Ibrahim1209`);
-      await client.sendMessage(m.chat, processingMessage.id, json.message); // Edit placeholder message with result
-      } catch (e) {
-        console.error(e);
-        return client.reply(m.chat, global.status.error, m);
-      }
+      
+      await client.sendMessageModify(m.chat, processingMessage.id, json.message); // Edit placeholder message with result
+    } catch (e) {
+      console.error(e);
+      return client.reply(m.chat, global.status.error, m);
+    }
   },
   error: false,
   limit: true,
   verified: true,
   premium: false,
-}
+};
