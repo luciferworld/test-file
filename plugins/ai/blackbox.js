98f37ceca5e54121987431373725e47c4517bc49
galalio445 <theworldh95@gmail.com>
2024-02-21 15:15:49 +0500

    test

--- plugins/ai/blackbox.js
+++ plugins/ai/blackbox.js
@@ -13,8 +13,9 @@ exports.run = {
        try {
         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
         client.sendReact(m.chat, '🕒', m.key)
+        const edit = awaitclient.reply(m.chat, 'in progress......', m)
         const json = await Func.fetchJson(`https://api.betabotz.eu.org/api/search/blackbox-chat?text=${text}&apikey=beta-Ibrahim1209`)
-        client.reply(m.chat, json.message, m)
+        client.sendEditMessage(m.chat, json.message, m)
         } catch (e) {
       return client.reply(m.chat, global.status.error, m)
   }
