e4f9730571cef25481b3af88facce473579830aa
galalio445 <theworldh95@gmail.com>
2024-02-21 15:18:56 +0500

    asf

--- plugins/ai/blackbox.js
+++ plugins/ai/blackbox.js
@@ -1,27 +1,27 @@
 exports.run = {
-    usage: ['blackbox'],
-    use: 'query',
-    category: 'ai',
-    async: async (m, {
-      client,
-      text,
-      args,
-      isPrefix,
-      command,
-      Func
-    }) => {
-       try {
-        if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
-        client.sendReact(m.chat, '🕒', m.key)
-        const edit = awaitclient.reply(m.chat, 'in progress......', m)
-        const json = await Func.fetchJson(`https://api.betabotz.eu.org/api/search/blackbox-chat?text=${text}&apikey=beta-Ibrahim1209`)
-        client.sendEditMessage(m.chat, json.message, {lastMessages: [edit] }, m)
-        } catch (e) {
+  usage: ['blackbox'],
+  use: 'query',
+  category: 'ai',
+  async: async (m, {
+    client,
+    text,
+    args,
+    isPrefix,
+    command,
+    Func
+  }) => {
+     try {
+      if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
+      client.sendReact(m.chat, '🕒', m.key)
+      const edit = await client.reply(m.chat, 'in progress......', m) // Added missing space after 'await'
+      const json = await Func.fetchJson(`https://api.betabotz.eu.org/api/search/blackbox-chat?text=${text}&apikey=beta-Ibrahim1209`) // Added missing space after 'await'
+      client.sendEditMessage(m.chat, json.message, {lastMessages: [edit] }, m) // Fixed typo in 'sendEditMessage'
+    } catch (e) {
       return client.reply(m.chat, global.status.error, m)
-  }
-},
-error: false,
-limit: true,
-verified: true,
-premium: false,
-}
+    }
+  },
+  error: false,
+  limit: true,
+  verified: true,
+  premium: false,
+};
