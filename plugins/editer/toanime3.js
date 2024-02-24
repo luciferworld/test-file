8b7447075b31ac572a5de58dbee8d20c04a27bb0
galalio445 <theworldh95@gmail.com>
2024-02-24 15:09:12 +0500

    asdfas

--- plugins/editer/toanime3.js
+++ plugins/editer/toanime3.js
@@ -1,6 +1,6 @@
-const {photoToCartoon} = require("haji-api/modules/image");
+const {googlePlayDownload} = require("haji-api/modules/googleplay");
 exports.run = {
-    usage: ['toanime3'],
+    usage: ['sssd'],
     use: 'reply photo',
     category: 'converter',
     async: async (m, {
@@ -11,29 +11,8 @@ exports.run = {
        Scraper
     }) => {
        try {
-          if (m.quoted ? m.quoted.message : m.msg.viewOnce) {
-             let type = m.quoted ? Object.keys(m.quoted.message)[0] : m.mtype
-             let q = m.quoted ? m.quoted.message[type] : m.msg
-             if (/image/.test(type)) {
-                client.sendReact(m.chat, '🕒', m.key)
-                let img = await client.downloadMediaMessage(q)
-                let image = await Scraper.uploadImageV2(img)
-                photoToCartoon({
-                    url: image.data.url
-                }).then(console.log);
-             } else client.reply(m.chat, Func.texted('bold', `🚩 Only for photo.`), m)
-          } else {
-             let q = m.quoted ? m.quoted : m
-             let mime = (q.msg || q).mimetype || ''
-             if (!mime) return client.reply(m.chat, Func.texted('bold', `🚩 Reply photo.`), m)
-             if (!/image\/(jpe?g|png)/.test(mime)) return client.reply(m.chat, Func.texted('bold', `🚩 Only for photo.`), m)
-             client.sendReact(m.chat, '🕒', m.key)
-             let img = await q.download()
-             let image = await Scraper.uploadImageV2(img)
-             photoToCartoon({
-                url: image.data.url
-            }).then(console.log);
-          }
+        googlePlayDownload({url: "https://play.google.com/store/apps/details?id=org.telegram.messenger"})
+        .then(console.log);
        } catch (e) {
           return client.reply(m.chat, Func.jsonFormat(e), m)
        }
