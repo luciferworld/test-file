58b49e5196406c31f1187cbaccbf1684b06a6dcb
galalio445 <theworldh95@gmail.com>
2024-02-25 13:26:54 +0500

    test

--- plugins/test.js
+++ plugins/test.js
@@ -1,4 +1,5 @@
 const { dalle } = require("gpti");
+
 exports.run = {
     usage: ['test'],
     category: 'beta',
@@ -11,19 +12,33 @@ exports.run = {
         text
     }) => {
         try {
-          dalle.mini({
-            prompt: "An extensive green valley stretches toward imposing mountains, adorned with meadows and a winding stream. The morning sun paints the sky with warm tones, illuminating the landscape with a serenity that invites contemplation and peace."
-        }, (err, data) => {
-            if(err != null){
-                console.log(err);
-            } else {
-                console.log(data);
-            }
-        });
-         // Fixed typo in 'sendEditMessage'
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'girl'), m);
+            
+            client.sendReact(m.chat, '🕒', m.key);
+            
+            dalle.mini({
+                prompt: `${text}`
+            }, (err, data) => {
+                if (err != null) {
+                    console.log(err);
+                    return client.reply(m.chat, global.status.error, m); // Notify user of error
+                }
+
+                const prompt = data.prompt; // Extract prompt from response
+                const images = data.images; // Extract images array from response
+
+                images.forEach((imageData, index) => {
+                    const buffer = Buffer.from(imageData.split(';base64,').pop(), 'base64');
+                    client.sendFile(m.chat, buffer, `image_${index + 1}.jpg`, `◦  *Prompt* : ${prompt}`, m);
+                });
+            });
         } catch (e) {
-          return client.reply(m.chat, global.status.error, m)
-    }
-     },
-    error: false
- }
+            console.error(e);
+            return client.reply(m.chat, global.status.error, m);
+        }
+    },
+    error: false,
+    limit: true,
+    premium: false,
+    verified: true,
+};
