e1ae61e49844a5bf9528be034dddcbe229bb049e
galalio445 <theworldh95@gmail.com>
2024-03-17 17:48:17 +0500

    fix

--- plugins/convert/qc.js
+++ plugins/convert/qc.js
@@ -41,7 +41,7 @@ exports.run = {
                "replyMessage": {}
             }]
          }
-         const json = await axios.post('https://quote-api.replit.app/generate', obj, {
+         const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {
             headers: {
                'Content-Type': 'application/json'
             }
@@ -59,6 +59,5 @@ exports.run = {
    error: false,
    limit: true,
    cache: true,
-   verified: true,
    location: __filename
 }
--- plugins/menu.js
+++ plugins/menu.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs')
 exports.run = {
    usage: ['menu', 'help', 'command'],
