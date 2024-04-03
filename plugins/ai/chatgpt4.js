b9d766d946f214b8d82c22ef49c611d26448d130
galalio445 <theworldh95@gmail.com>
2024-04-03 16:44:55 +0500

    asd

--- plugins/ai/chatgpt4.js
+++ plugins/ai/chatgpt4.js
@@ -1,41 +1,53 @@
-const { gpt } = require("gpti");
+const { G4F } = require("g4f");
+const g4f = new G4F();
+
+// Create an array to store the conversation
+let conversation = [];
 
 exports.run = {
-   usage: ['chatgpt4'],
-   use: 'prompt',
-   category: 'ai',
-   async: async (m, {
-      client,
-      text,
-      isPrefix,
-      command,
-      Func
-   }) => {
-      try {
-         if (!m.quoted && !text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m);
+    usage: ['test'],
+    use: 'prompt',
+    category: 'ai',
+    async: async (m, {
+       client,
+       text,
+       isPrefix,
+       command,
+       Func
+    }) => {
+       try {
+        if (!m.quoted && !text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m);
          
-         client.sendReact(m.chat, '🕒', m.key);
-         
-         gpt({
-            messages: [
-                 ],
-            prompt: `${text}`,
-            model: "GPT-4",
-            markdown: false
-        }, (err, data) => {
-            if(err != null){
-                console.log(err);
-            } else {
-                m.reply(data.gpt);
-            }
-        });
-      } catch (e) {
-         client.reply(m.chat, Func.jsonFormat(e), m);
-      }
-   },
-   error: false,
-   limit: true,
-   cache: true,
-   verified: true,
-   location: __filename
+        client.sendReact(m.chat, '🕒', m.key);
+
+        // If there's text, add user's message to the conversation array
+        if (text) {
+            conversation.push({ userId: `${m.jid}`, role: "user", content: `${text}` });
+        }
+
+        const options = {
+            provider: g4f.providers.GPT,
+            model: "gpt-4",
+            debug: true,
+            proxy: ""
+        };
+        
+        (async() => {
+            // Use the conversation array as input for chatCompletion method
+            const resp = await g4f.chatCompletion(conversation, options);    
+            m.reply(resp); 
+
+            // Add the AI response to the conversation array
+            conversation.push({ role: "assistant", content: resp });
+        })();
+          
+       } catch (e) {
+          client.reply(m.chat, Func.jsonFormat(e), m);
+       }
+    },
+    error: false,
+    limit: true,
+    cache: true,
+    verified: true,
+    location: __filename
 };
