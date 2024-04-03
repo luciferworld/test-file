86437c47c4bbd16608d53a74188c342a24afc6a6
galalio445 <theworldh95@gmail.com>
2024-04-03 16:35:15 +0500

    fix

---
+++ plugins/ai/test.js
@@ -0,0 +1,43 @@
+const { G4F } = require("g4f");
+const g4f = new G4F();
+exports.run = {
+    usage: ['test'],
+    use: 'prompt',
+    category: 'ai',
+    async: async (m, {
+       client,
+       text,
+       isPrefix,
+       command,
+       Func
+    }) => {
+       try {
+         
+        const messages = [
+            { userId: `${m.jid}`, role: "user", content: "Hi, what's up?"}
+            // Add more messages here if needed
+        ];
+        const options = {
+            provider: g4f.providers.GPT,
+            model: "gpt-4",
+            debug: true,
+            proxy: ""
+        };
+        
+        (async() => {
+            const text = await g4f.chatCompletion(messages, options);    
+            console.log(text); 
+        })();s
+          
+          
+       } catch (e) {
+          client.reply(m.chat, Func.jsonFormat(e), m);
+       }
+    },
+    error: false,
+    limit: true,
+    cache: true,
+    verified: true,
+    location: __filename
+ };
+ 
