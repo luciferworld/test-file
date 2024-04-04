5f578b3f29ed48ef8676aae8759d4e2bac4f8ded
galalio445 <theworldh95@gmail.com>
2024-04-05 02:11:13 +0500

    sad

---
+++ plugins/ai/mygf.js
@@ -0,0 +1,28 @@
+import ChatBot from "sydney-ai";
+const bot = new ChatBot(`${global.bing}`);
+xports.run = {
+    usage: ['mygf'],
+    use: 'query ',
+    category: 'ai',
+    async: async (m, {
+      client,
+      text,
+      args,
+      isPrefix,
+      command,
+      Func
+    }) => {
+       try {
+        if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'what is java script'), m)
+         client.sendReact(m.chat, '🕒', m.key)
+         let response = await bot.ask(text);
+        m.reply(response)
+        } catch (e) {
+      return client.reply(m.chat, global.status.error, m)
+  }
+},
+error: false,
+limit: true,
+verified: true,
+premium: false,
+}
