67b19daa789978e8705a7dd37390275b37fca19c
galalio445 <theworldh95@gmail.com>
2024-04-09 15:55:39 +0500

    asdfasdf

--- plugins/search/apkserach.js
+++ plugins/search/apkserach.js
@@ -1,29 +1,30 @@
-const gplay = require('google-play-scraper')
+const gplay = await import('google-play-scraper');
+
 exports.run = {
     usage: ['apksearch'],
     use: 'query',
     category: 'search',
     async: async (m, {
-      client,
-      text,
-      args,
-      isPrefix,
-      command,
-      Func
+        client,
+        text,
+        args,
+        isPrefix,
+        command,
+        Func
     }) => {
-       try {
-        if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'Facebook'), m)
+        try {
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'Facebook'), m)
             client.sendReact(m.chat, '🕒', m.key)
             gplay.search({
-              term: text,
-              num: 2
+                term: text,
+                num: 2
             }).then(console.log, console.log);
-       } catch (e) {
-      return client.reply(m.chat, global.status.error, m)
-  }
-  },
-error: false,
-limit: true,
-verified: true,
-premium: false
+        } catch (e) {
+            return client.reply(m.chat, global.status.error, m)
+        }
+    },
+    error: false,
+    limit: true,
+    verified: true,
+    premium: false
 }
