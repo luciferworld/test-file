43f76f92cb04d325fe24a56825b464c9d609a4a4
galalio445 <theworldh95@gmail.com>
2024-04-09 18:02:54 +0500

    asdf

--- plugins/download/app.js
+++ plugins/download/app.js
@@ -1,15 +1,21 @@
 const {download} = require('aptoide-scraper')
+const gplay = require('custom-google-play-scraper');
 exports.run = {
     usage: ['app'],
-    use: 'app id',
+    use: 'Name',
     category: 'downloader',
     async: async (m, { client, args, text, isPrefix, command, Func }) => {
         try {
             if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'your app id(com.whatsapp)'), m);
 
             client.sendReact(m.chat, '🕒', m.key);
+            const response = await gplay.search({
+                term: text,
+                num: 2,
+                throttle: 10
+              });
 
-            let data = await download(text);
+            let data = await download(response[0].appId);
             let teks = `乂  *A P K  D O W N L O A D E R *\n\n`
             teks += '	◦  *Name* : ' + data.name + '\n'
             teks += '	◦  *Upated on*: ' + data.lastup + '\n'
