aad1ee4559174ac42daad37a9534a76b2d5e1200
galalio445 <theworldh95@gmail.com>
2024-04-09 17:20:48 +0500

    asd

--- plugins/search/apkserach.js
+++ plugins/search/apkserach.js
@@ -10,12 +10,20 @@ exports.run = {
       
       client.sendReact(m.chat, '🕒', m.key);
       
-       gplay.search({
+      const response = await gplay.search({
         term: text,
         num: 2
-      }).then((response) => {
-        console.log(response);
       });
+
+      let combinedCaption = '乂  *Y T  S E R A C H*\n\n';
+      response.forEach((v, index) => {
+        combinedCaption += `    ◦  *Name*: ${v.title}\n`;
+        combinedCaption += `    ◦  *AppID*: ${v.appId}\n`;
+        combinedCaption += `    ◦  *URL*: ${v.url}\n\n`;
+      });
+
+      await m.reply(combinedCaption); // Await the reply to ensure it's sent after processing
+
     } catch (e) {
       console.error(e); // Log the error for debugging
       return client.reply(m.chat, global.status.error, m);
