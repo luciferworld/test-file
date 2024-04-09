564b08ac51c3b6353dc8cdc168b07645c16cc1d2
galalio445 <theworldh95@gmail.com>
2024-04-09 19:37:50 +0500

    asdfasdf

--- plugins/download/app.js
+++ plugins/download/app.js
@@ -1,17 +1,16 @@
 const { download, search } = require('aptoide-scraper');
 
 exports.run = {
-    usage: ['app', 'appdl'],
+    usage: ['app'],
     use: 'app id',
     category: 'downloader',
     async: async (m, { client, args, text, isPrefix, command, Func }) => {
         try {
-            if (command === 'appdl') {
-                if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'your app id(com.whatsapp)'), m);
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'your app id(com.whatsapp)'), m);
 
                 client.sendReact(m.chat, '🕒', m.key);
-
-                let ssss = await download(text);
+                let results = await search(text);
+                let ssss = await download(results[0].id);
                 let teks = `乂  *A P K  D O W N L O A D E R *\n\n`
                 teks += '	◦  *Name* : ' + ssss.name + '\n'
                 teks += '	◦  *Upated on*: ' + ssss.lastup + '\n'
@@ -20,24 +19,7 @@ exports.run = {
                 client.sendFile(m.chat, ssss.icon, '', teks, m).then(() => {
                     client.sendFile(m.chat, ssss.dllink, ssss.name + '.apk', ssss.name, m)
                 });
-            } else if (command === 'app') {
-                if (!text) return client.reply(m.chat, 'Please provide an app name to search for.', m);
-
-                let results = await search(text);
-                let teks = `乂  *A P K  D O W N L O A D E R *\n\n`;
-
-                if (results.length > 0) {
-                    results.forEach(result => {
-                        teks += '	◦  *Name* : ' + result.name + '\n';
-                        teks += '	◦  *AppID*: ' + result.id + '\n';
-                        teks += '	◦  *Rating*: ' + result.rating + '\n\n';
-                    });
-                } else {
-                    teks += 'No results found.';
-                }
-
-                client.reply(m.chat, teks, m);
-            }
+           
         } catch (e) {
             console.error(e);
             return client.reply(m.chat, global.status.error, m);
