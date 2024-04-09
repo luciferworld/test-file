cf1e942dd43cf3a92caca2e13d1b9e43177520d6
galalio445 <theworldh95@gmail.com>
2024-04-09 15:45:25 +0500

    asd

--- plugins/search/apkserach.js
+++ plugins/search/apkserach.js
@@ -1,3 +1,4 @@
+const gplay = require('google-play-scraper')
 exports.run = {
     usage: ['apksearch'],
     use: 'query',
@@ -13,19 +14,10 @@ exports.run = {
        try {
         if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'Facebook'), m)
             client.sendReact(m.chat, '🕒', m.key)
-            let json = await Func.fetchJson(`https://api.akuari.my.id/search/playstoresearch?query=${text}`)  
-            let textt = "乂  *APK Search*\n\nResult From search  " + text + "\n\nTo download type /apkdl your app id\n\n";
-            json.hasil.slice(0, 2).map(async (v, i) => {
-
-           textt += `	◦  *Name* : ${v.title}\n	◦  *App Id* : ${
-          v.appId
-        }\n	◦  *Developer* : ${
-          v.developer
-        }\n	◦  *Link* : ${
-          v.url
-        }\n\n`;
-            })
-           client.sendFile(m.chat, json.hasil[0].icon, '', textt, m)
+            gplay.search({
+              term: text,
+              num: 2
+            }).then(console.log, console.log);
        } catch (e) {
       return client.reply(m.chat, global.status.error, m)
   }
