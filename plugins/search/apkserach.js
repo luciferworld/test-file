0ce613c5dc21475ff26aa2993fd010a5fe7570f4
galalio445 <theworldh95@gmail.com>
2024-04-09 17:13:44 +0500

    asdf

--- plugins/search/apkserach.js
+++ plugins/search/apkserach.js
@@ -18,11 +18,19 @@ exports.run = {
         term: `${text}`,
         num: 2
       })
-      let textt = '乂  *P l A Y S T O R E  S E R A C H*\n\n'
-      textt += `    ◦  *Name* : ${json.title}\n`
-      textt += `    ◦  *AppID* : ${json.appId}\n`
-      textt += `    ◦  *Link* : ${json.url}\n`
-      client.sendFile(m.chat, json.icon, '', textt, m)
+      const results = json
+            const chunkSize = 18;
+            for (let i = 0; i < results.length; i += chunkSize) {
+                const chunk = results.slice(i, i + chunkSize);
+                let combinedCaption = i === 0 ? '乂  *Y T  S E R A C H*\n\n' : ''; // Include caption only for the first chunk
+                chunk.forEach((v, index) => {
+                    combinedCaption += `    ◦  *Nane* : ${v.title}\n`;
+                    combinedCaption += `    ◦  *AppID* : ${v.appId}\n`;
+                    combinedCaption += `    ◦  *URL* : ${v.url}\n\n`;
+                });
+                // Send the combined caption with search results as a single message
+                await m.reply(combinedCaption);
+            }
     } catch (e) {
       return client.reply(m.chat, global.status.error, m)
     }
