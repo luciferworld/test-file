4e93442702cc31ecf7430e74c1ea41704109c973
galalio445 <theworldh95@gmail.com>
2024-04-10 17:13:34 +0500

    asdf

--- plugins/download/terabox.js
+++ plugins/download/terabox.js
@@ -6,22 +6,18 @@ exports.run = {
     category: 'downloader',
     async: async (m, { client, args, isPrefix, command, Func, text }) => {
         try {
-            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'your link'), m);
-            client.sendReact(m.chat, '🕒', m.key);
+            const { TeraDood } = require('@kodingkeundev/teradood')
 
-            const response = await axios.get(`https://rest-api.akuari.my.id/downloader/teraboxdl?link=${text}`);
-            const jsonData = response.data;
-            
-            // Check if response contains download links
-            if (!jsonData || !jsonData.linkdl || !jsonData.linkdl.length) {
-                return client.reply(m.chat, 'Check your link its wrong', m);
-            }
-
-            // Get the first download link
-            const downloadLink = jsonData.linkdl[0].link;
-
-            // Send the file to the chat
-            client.sendFile(m.chat, downloadLink, '', '', m);
+            var url = 'https://teraboxapp.com/s/1heSERcqw_avia6RFOIzoSg'
+            TeraDood.terabox(url)
+              .then(result => {
+                // action here
+                console.log(result) // for see details result
+              })
+              .catch(error => {
+                // action error here
+                console.error('Something went wrong', error) // for see details error
+              })
         } catch (e) {
             console.error(e);
             return client.reply(m.chat, global.status.error, m);
