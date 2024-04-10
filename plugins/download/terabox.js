13a354cdb4d0c89e4a25ee8a4235010490c830e5
galalio445 <theworldh95@gmail.com>
2024-04-10 17:17:41 +0500

    sasd

--- plugins/download/terabox.js
+++ plugins/download/terabox.js
@@ -1,4 +1,4 @@
-const { TeraDood } = require('@kodingkeundev/teradood')
+const axios = require('axios');
 
 exports.run = {
     usage: ['terabox'],
@@ -6,16 +6,22 @@ exports.run = {
     category: 'downloader',
     async: async (m, { client, args, isPrefix, command, Func, text }) => {
         try {
+            if (!text) return client.reply(m.chat, Func.example(isPrefix, command, 'your link'), m);
+            client.sendReact(m.chat, '🕒', m.key);
 
-            var url = 'https://teraboxapp.com/s/1heSERcqw_avia6RFOIzoSg'
-            TeraDood.terabox(url)
-                .then(result => {
-                    console.log(result) // for see details result
-                })
-                .catch(error => {
-                    // action error here
-                    console.error('Something went wrong', error) // for see details error
-                })
+            const response = await axios.get(`https://rest-api.akuari.my.id/downloader/teraboxdl?link=${text}`);
+            const jsonData = response.data;
+            
+            // Check if response contains download links
+            if (!jsonData || !jsonData.linkdl || !jsonData.linkdl.length) {
+                return client.reply(m.chat, 'Check your link its wrong', m);
+            }
+
+            // Get the first download link
+            const downloadLink = jsonData.linkdl[0].link;
+
+            // Send the file to the chat
+            client.sendFile(m.chat, downloadLink, '', '', m);
         } catch (e) {
             console.error(e);
             return client.reply(m.chat, global.status.error, m);
