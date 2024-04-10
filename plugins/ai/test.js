2c2e4bf862fa94b21a49b8df5f4cbd92e4edb1f3
galalio445 <theworldh95@gmail.com>
2024-04-10 21:54:26 +0500

    asdf

--- plugins/ai/test.js
+++ plugins/ai/test.js
@@ -4,38 +4,41 @@ exports.run = {
     use: 'prompt',
     category: 'ai',
     async: async (m, {
-       client,
-       text,
-       isPrefix,
-       command,
-       Func
+        client,
+        text,
+        isPrefix,
+        command,
+        Func
     }) => {
-       try {
-          
-          
-          
-bing({
-    messages: [
-                  ],
-             prompt: `Hi`,
-             conversation_style: "Balanced",
-             markdown: false,
-             stream: false,
-         }, (err, data) => {
-             if(err != null){
-                 console.log(err);
-             } else {
-                 console.log(data);
-             }
-         });
-       } catch (e) {
-          client.reply(m.chat, Func.jsonFormat(e), m);
-       }
+        try {
+
+
+
+            bing({
+                messages: [
+                    {
+                        role: "user",
+                        content: "Can you tell me how many movies you've told me about?"
+                    }
+                ],
+
+                conversation_style: "Balanced",
+                markdown: false,
+                stream: false,
+            }, (err, data) => {
+                if (err != null) {
+                    console.log(err);
+                } else {
+                    console.log(data);
+                }
+            });
+        } catch (e) {
+            client.reply(m.chat, Func.jsonFormat(e), m);
+        }
     },
     error: false,
     limit: true,
     cache: true,
     verified: true,
     location: __filename
- };
- 
+};
