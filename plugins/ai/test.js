7680f501011892dc356013e997b67965654a8afe
galalio445 <theworldh95@gmail.com>
2024-04-10 21:47:22 +0500

    sadf

---
+++ plugins/ai/test.js
@@ -0,0 +1,42 @@
+const { bing } = require("gpti");
+exports.run = {
+    usage: ['test'],
+    use: 'prompt',
+    category: 'ai',
+    async: async (m, {
+       client,
+       text,
+       isPrefix,
+       command,
+       Func
+    }) => {
+       try {
+          
+          
+          
+bing({
+    messages: [
+                  ],
+             prompt: Hi,
+             model: "GPT-4",
+             conversation_style: "Balanced",
+    markdown: false,
+    stream: false,
+}, (err, data) => {
+    if(err != null){
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
+       } catch (e) {
+          client.reply(m.chat, Func.jsonFormat(e), m);
+       }
+    },
+    error: false,
+    limit: true,
+    cache: true,
+    verified: true,
+    location: __filename
+ };
+ 
